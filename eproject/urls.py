39abb2c65910264c75788d51fadf78ac5be91dc9
Simon123-lab <63109996+Simon123-lab@users.noreply.github.com>
2021-10-17 15:47:42 +0500

    Update urls.py

--- eproject/urls.py
+++ eproject/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path,include
 from django.views.static import serve
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
